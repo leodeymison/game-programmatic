adade5268e0661fb2688d935387bc9ed1e3732c8
leodeymison <leodeymisonbasquete@gmail.com>
2025-10-11 17:15:30 -0300

    start bd prisma

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,4 +1,10 @@
 import { NextResponse } from "next/server";
+import { prisma } from "../../lib/prisma";
+
+export async function GET() {
+  const users = await prisma.user.findMany();
+  return NextResponse.json(users);
+}
 
 export async function POST(request: Request) {
   const body = await request.json();
